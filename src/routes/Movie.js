fc1c14e2883019e875935e2e318ee047c0e97682
subbi <92204014+subinsad@users.noreply.github.com>
2023-12-17 22:46:12 +0900

    주석추가

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -15,7 +15,7 @@ export default class Movie extends Component {
         await getMovieDetails(history.state.id);
         console.log(movieStore.state.movie);
         const { movie } = movieStore.state;
-        const bigPoster = movie.Poster.replace('SX300', 'SX700');
+        const bigPoster = movie.Poster.replace('SX300', 'SX700'); //화질변경
 
         this.el.innerHTML = /* html */ `
         <div 
